5b029d4105ead494666e337e62fd66d1e49465a0
Rb2030 <rossmail2050@gmail.com>
2016-06-13 17:50:09 +0100

    added link feature

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra/base'
+require_relative './lib/link'
+
+class BookmarkManager < Sinatra::Base
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  run! if app_file == $0
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
 require 'capybara/rspec'
-require './lib/link'
+require 'link'
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'rspec'
+require 'capybara'
+
+Capybara.app = BookmarkManager
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
